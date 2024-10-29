53c9374fa85ad64b4d8ccbf11ce9fa4f854c5d82
Mykhailo Stryzhka <mykhailo.stryzhka@seznam.cz>
2024-10-29 18:47:35 +0100

    Update registerFormScheme.js

--- src/schemas/registerFormScheme.js
+++ src/schemas/registerFormScheme.js
@@ -1,15 +1,20 @@
 import * as Yup from 'yup';
 
 export const validationRegisterFormScheme = Yup.object().shape({
-  id: Yup.string().matches(/^\d{5}$/, 'id must be exactly 5 digits'),
+  id: Yup.string().required('Required field'),
   name: Yup.string(),
   surname: Yup.string(),
   email: Yup.string()
     .required('Required field')
     .email('Enter a valid email address'),
   password: Yup.string()
-    .required('Pin is required')
-    .matches(/^\d{6}$/, 'Pin must be exactly 6 digits'),
+    .required('Required field')
+    .min(8, 'Password must be at least 8 characters')
+    .max(32, 'Password must be less than 32 characters')
+    .matches(
+      /^(?=.*[A-Z])(?=.*\d).{8,16}$/,
+      'Password must contain at least 8 Latin characters, one uppercase letter, one digit'
+    ),
   phoneNumber: Yup.string()
     .required('Phone number is required')
     .matches(/^(?:\+420)?\d{9}$/, 'Enter a valid Czech phone number'),
