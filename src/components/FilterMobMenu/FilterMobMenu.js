bc213e102741992092b923049e2ce0d9819f4e40
Mykhailo Stryzhka <mykhailo.stryzhka@seznam.cz>
2024-10-29 15:51:23 +0100

    Update FilterMobMenu.js

--- src/components/FilterMobMenu/FilterMobMenu.js
+++ src/components/FilterMobMenu/FilterMobMenu.js
@@ -1,10 +1,9 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import {
   Box,
   IconButton,
   VStack,
   Checkbox,
-  Select,
   Button,
   useDisclosure,
   Menu,
