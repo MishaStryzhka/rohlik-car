7b9c52fcff8e6dca089b1b6c6a6e073026e53d53
Mykhailo Stryzhka <mykhailo.stryzhka@seznam.cz>
2024-10-30 22:52:21 +0100

    Update BarCode.js

--- src/pages/BarCode.js
+++ src/pages/BarCode.js
@@ -1,17 +1,26 @@
 import {
+  Button,
   Container,
   Flex,
   Input,
   InputGroup,
   InputLeftElement,
 } from '@chakra-ui/react';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import Barcode from 'react-barcode';
 import { Helmet } from 'react-helmet';
 import { CiBarcode } from 'react-icons/ci';
 
 const BarCode = () => {
+  const inputRef = useRef(null);
   const [barCode, setBarCode] = useState('1091561721-DN');
+
+  const isFull =
+    barCode.includes('-A-') ||
+    barCode.includes('-C-') ||
+    barCode.includes('-F-') ||
+    barCode.includes('-DN');
+
   return (
     <>
       <Helmet>
@@ -24,7 +33,8 @@ const BarCode = () => {
             <CiBarcode color="gray.300" />
           </InputLeftElement>
           <Input
-            w={{ base: '100%', md: '400px' }}
+            ref={inputRef}
+            w={{ base: '100%', md: '100%' }}
             value={barCode}
             onChange={e => {
               setBarCode(e.target.value.toUpperCase());
@@ -32,6 +42,72 @@ const BarCode = () => {
             placeholder="Search"
           />
         </InputGroup>
+        <Flex p="10px 50px" justify="space-evenly">
+          <Button
+            size="sm"
+            bg="#6da305"
+            color="white"
+            _hover={{ bg: '#8d8d8d' }}
+            onClick={() => {
+              setBarCode(prev => prev + '-A-');
+              inputRef.current.focus();
+            }}
+            aria-label="Open Modal Update Coment"
+            position="relative"
+            gap={1}
+            disabled={isFull}
+          >
+            -A-
+          </Button>
+          <Button
+            size="sm"
+            bg="#6da305"
+            color="white"
+            _hover={{ bg: '#8d8d8d' }}
+            onClick={() => {
+              setBarCode(prev => prev + '-C-');
+              inputRef.current.focus();
+            }}
+            aria-label="Open Modal Update Coment"
+            position="relative"
+            gap={1}
+            disabled={isFull}
+          >
+            -C-
+          </Button>
+          <Button
+            size="sm"
+            bg="#6da305"
+            color="white"
+            _hover={{ bg: '#8d8d8d' }}
+            onClick={() => {
+              setBarCode(prev => prev + '-F-');
+              inputRef.current.focus();
+            }}
+            aria-label="Open Modal Update Coment"
+            position="relative"
+            gap={1}
+            disabled={isFull}
+          >
+            -F-
+          </Button>
+          <Button
+            size="sm"
+            bg="#6da305"
+            color="white"
+            _hover={{ bg: '#8d8d8d' }}
+            onClick={() => {
+              setBarCode(prev => prev + '-DN');
+              inputRef.current.focus();
+            }}
+            aria-label="Open Modal Update Coment"
+            position="relative"
+            gap={1}
+            disabled={isFull}
+          >
+            -DN
+          </Button>
+        </Flex>
         <Flex justify="center" mt={8}>
           <Barcode
             value={barCode} // Значення для генерації штрих-коду
